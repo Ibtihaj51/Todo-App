0f16928e4b6383f0fea01205d7f2c8168682cf18
Muhammad Abdullah <equamarlex@gmail.com>
2022-09-26 19:29:48 +0500

    Todo

---
+++ server.mjs
@@ -0,0 +1,31 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+let todos = [];
+
+app.use(express.json());
+app.use(cors())
+
+app.post('/todo', (req, res) => {
+    
+    todos.push(req.body.text);
+
+    res.send({
+        message: "your todo is saved",
+        data: todos
+    })
+})
+app.get('/todos', (req, res) => {
+    
+    res.send({
+        message: "Here is your Todo List",
+        data: todos
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
